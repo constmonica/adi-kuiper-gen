2eba2cba341961434dda8da95824f211ae48d46a
Mircea Caprioru <mircea.caprioru@analog.com>
2020-05-27 12:31:29 +0300

    stage6: Move cosmetic changes to stage6
    This patch leaves stage7 in charge with the Linux Kernel build and moves
    the cosmetic changes to stage6.
    
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

--- stage6/06-cosmetic/00-run.sh
+++ stage6/06-cosmetic/00-run.sh
@@ -5,15 +5,5 @@ cp -f files/launch.png "${ROOTFS_DIR}/usr/share/raspberrypi-artwork"
 
 on_chroot << EOF
 sed -i 's+^wallpaper=.*$+wallpaper=/usr/share/rpd-wallpaper/wallpaper.png+g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
-sed -i 's+start-here+adi-colorimeter+g' /etc/xdg/lxpanel/LXDE-pi/panels/panel
-
-#disable screensaver
-sed -i 's+@xscreensaver -no-splash+#@xscreensaver -no-splash+g' /etc/xdg/lxsession/LXDE-pi/autostart
-echo "@xset s off" >> /etc/xdg/lxsession/LXDE-pi/autostart
-echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
-
-rm -f /usr/share/X11/xorg.conf.d/99-fbturbo.conf
-
-#mark raspberrypi-ui-mods to hold
-apt-mark hold raspberrypi-ui-mods
+sed -i 's+start-here+/usr/share/raspberrypi-artwork/launch.png+g' /etc/xdg/lxpanel/LXDE-pi/panels/panel
 EOF

1b52286a44c01461e6b8374d5fe1fc8f578f4404
stefan.raus <stefan.raus@analog.com>
2022-05-20 08:35:34 +0100

    stage6:04-gnuradio-m2k: fix iio import in gnuradio
    Update gnuradio-grc.desktop to open Gnu Radio from menu with parameter
    "PYTHONPATH=usr/local/lib/python3/dist-packages:/lib/python3.9/site-packages"
    This will load correct iio, gr-iio instead of ADI libiio.
    
    Fix also a typo in PYTHONPATH variable from bashrc.
    
    Signed-off-by: stefan.raus <stefan.raus@analog.com>

--- stage6/03-pyadi/00-run.sh
+++ stage6/03-pyadi/00-run.sh
@@ -4,7 +4,7 @@ on_chroot << EOF
 
 pip3 install pyadi-iio
 pip3 install git+https://github.com/analogdevicesinc/pyadi-dt.git
-echo "export PYTHONPATH=\"${PYTHONPATH}:/usr/local/lib/python3/dist-package:/lib/python3.9/site-packages\"" >> /home/analog/.bashrc
+echo "export PYTHONPATH=\"${PYTHONPATH}:/usr/local/lib/python3/dist-packages:/lib/python3.9/site-packages\"" >> /home/analog/.bashrc
 echo "export LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}:/usr/local/lib\"" >> /home/analog/.bashrc
 ldconfig
 EOF

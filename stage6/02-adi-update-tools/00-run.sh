c2861f7f2c0985dfa6f3d0997306e624823a2e7b
Stefan Raus <Stefan.Raus@analog.com>
2023-06-15 13:33:24 +0100

    Switch GTK2 with GTK3 and install gtkdatabox-1.0.0 instead of default(0.9.3.1)

--- stage6/02-adi-update-tools/00-run.sh
+++ stage6/02-adi-update-tools/00-run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+   wget https://downloads.sourceforge.net/project/gtkdatabox/gtkdatabox-1/gtkdatabox-1.0.0.tar.gz
+   tar xvf gtkdatabox-1.0.0.tar.gz
+   cd gtkdatabox-1.0.0
+   ./configure
+   make install
+
+
 [ -d "linux_image_ADI-scripts" ] || {
 	git clone https://github.com/analogdevicesinc/linux_image_ADI-scripts
 }

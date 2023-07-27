fb86832d5d4ce18ade756b11c596a564aa409883
mlradu <larisa.radu@analog.com>
2023-07-27 16:05:58 +0300

    stage6: 06-cosmetic: Set wallpaper for second monitor

--- stage6/06-cosmetic/00-run.sh
+++ stage6/06-cosmetic/00-run.sh
@@ -5,6 +5,7 @@ cp -f files/launch.png "${ROOTFS_DIR}/usr/share/raspberrypi-artwork"
 
 on_chroot << EOF
 sed -i 's+^wallpaper=.*$+wallpaper=/usr/share/rpd-wallpaper/wallpaper.png+g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
+sed -i 's+^wallpaper=.*$+wallpaper=/usr/share/rpd-wallpaper/wallpaper.png+g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-1.conf
 sed -i 's+start-here+adi-colorimeter+g' /etc/xdg/lxpanel/LXDE-pi/panels/panel
 
 #disable screensaver

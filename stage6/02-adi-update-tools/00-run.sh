48c55a56a991a97c2a0adac39a053d376ec49f6d
Stefan Raus <Stefan.Raus@analog.com>
2024-01-23 12:28:29 +0000

    stage6/02-adi-updatet-tools:remove databox archive
    Remove gtkdatabox-1.0.0.tar.gz after is unpacked and installed because
    everytime there adi_update_tools.sh is run a new copy will be downloaded
    with a number appended at the end (ex. gtkdatabox-1.0.0.tar.gz.1)
    Fix also indentation.
    
    Signed-off-by: Stefan Raus <Stefan.Raus@analog.com>

--- stage6/02-adi-update-tools/00-run.sh
+++ stage6/02-adi-update-tools/00-run.sh
@@ -6,7 +6,7 @@ on_chroot << EOF
    cd gtkdatabox-1.0.0
    ./configure
    make install
-
+   rm -rf gtkdatabox-1.0.0.tar.gz
 
    [ -d "linux_image_ADI-scripts" ] || {
 	git clone https://github.com/analogdevicesinc/linux_image_ADI-scripts -b main

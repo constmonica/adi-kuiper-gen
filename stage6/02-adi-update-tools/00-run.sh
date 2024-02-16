fb3c307bed78ec47cd5ca8a156cda3a70f2ec986
Stefan Raus <Stefan.Raus@analog.com>
2024-02-16 10:51:33 +0000

    stage6/02-adi-update-tools: fix databox deletion
    The command 'rm -rf gtkdatabox-1.0.0.tar.gz' is run inside the folder
    gtkdatabox-1.0.0 and there is no archive to delete. cd one level up
    and call rm command only afterwards. Switch also from cd to pushd/popd.
    
    Signed-off-by: Stefan Raus <Stefan.Raus@analog.com>

--- stage6/02-adi-update-tools/00-run.sh
+++ stage6/02-adi-update-tools/00-run.sh
@@ -3,9 +3,12 @@
 on_chroot << EOF
    wget https://downloads.sourceforge.net/project/gtkdatabox/gtkdatabox-1/gtkdatabox-1.0.0.tar.gz
    tar xvf gtkdatabox-1.0.0.tar.gz
-   cd gtkdatabox-1.0.0
+
+   pushd gtkdatabox-1.0.0
    ./configure
    make install
+   popd
+
    rm -rf gtkdatabox-1.0.0.tar.gz
 
    [ -d "linux_image_ADI-scripts" ] || {
@@ -16,7 +19,6 @@ on_chroot << EOF
    git checkout main
    chmod +x adi_update_tools.sh
    ./adi_update_tools.sh dev
-
    popd
 
 EOF
